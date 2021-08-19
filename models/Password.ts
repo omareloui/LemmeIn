593497206c640cf92e31861563cbb99675decf6c
Omar Hussein <omarelwy@hotmail.com>
2021-08-20 01:37:13 +0200

    🗃️ Update password model

--- models/Password.ts
+++ models/Password.ts
@@ -1,15 +1,18 @@
 import db from "../config/db.ts";
 
+import { Bson } from "../deps.ts";
+
 export interface PasswordSchema {
   _id: { $oid: string };
+  userId: Bson.ObjectId;
   password: string;
   title: string;
+  emailOrUsername: string;
   site: string;
   note: string;
   icon: string;
-  oAuthParty: string; // make a lookup?
-  tags: string[]; // make a lookup?
-  // isOAuth: boolean; // make it a virtual
+  oAuthParty: Bson.ObjectId;
+  tags: Bson.ObjectId[];
 }
 
 const passwords = db.collection<PasswordSchema>("passwords");
