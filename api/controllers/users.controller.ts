1e7feddfd77eeb872ca16ac42b8231fae04756f4
Omar Hussein <omarelwy@hotmail.com>
2021-08-21 20:42:09 +0200

    🚧 Start on the register method, with a fail on running the script

--- api/controllers/users.controller.ts
+++ api/controllers/users.controller.ts
@@ -1,12 +1,49 @@
-import User from "../models/User.ts";
+import User, { UserSchema } from "../models/User.ts";
 
+import { createJWT, getNumericDate, hash } from "../deps.ts";
 import ControllerHelper from "./lib/ControllerHelper.ts";
+import APIError from "../lib/APIError.ts";
 
 const UserControllerHelper = new ControllerHelper(User, {
-  requiredFields: ["email", "password"],
+  requiredFields: ["email", "password", "username", "name"],
 });
 
 export default class UserController {
+  async register(data: UserSchema) {
+    const jwtSecret = Deno.env.get("JWT_TOKEN");
+
+    const key = await crypto.subtle.generateKey({ name: "HMAC", hash: "SHA-512" }, true, [
+      "sign",
+      "verify",
+    ]);
+
+    if (!jwtSecret) throw new APIError("Something went wrong, please try again later", 500);
+    const {
+      username,
+      email,
+      password,
+      name: { first, last },
+    } = data;
+    if (!username || !email || !password || !first || !last)
+      throw new APIError("All the fields has to be fill.", 400);
+    const hashedPassword = await hash(password);
+    // TODO: make sure the username and the email are unique
+    const insertId = await User.insertOne({ ...data, password: hashedPassword });
+    const expiration = getNumericDate(60 * 60);
+    const token = await createJWT(
+      { alg: "RS512", typ: "JWT" },
+      { userId: insertId, exp: getNumericDate(60 * 60) },
+      key
+    );
+
+    console.log({ key });
+    console.log({ expiration });
+    console.log({ token });
+    return { insertId, token };
+  }
+
+  async signin() {}
+
   async viewAll() {
     return await UserControllerHelper.viewAll();
   }
--- api/deps.ts
+++ api/deps.ts
@@ -3,3 +3,5 @@ export { Application, Router, Context } from "https://deno.land/x/oak@v9.0.0/mod
 export { MongoClient, Bson, Collection } from "https://deno.land/x/mongo@v0.25.0/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 export { Aes256Cfb8 } from "https://deno.land/x/aes_cfb@1.3/mod.ts";
+export { create as createJWT, getNumericDate } from "https://deno.land/x/djwt@v2.3/mod.ts";
+export { hash, verify as verifyHash } from "https://deno.land/x/argon2@v0.9.2/lib/mod.ts";
