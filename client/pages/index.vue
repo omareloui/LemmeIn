1aa68e14c92b01282037011dd07d49165674b429
Omar Hussein <omarelwy@hotmail.com>
2021-09-08 10:26:40 +0200

    💄 Set fonts and basic colors for the app

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -3,6 +3,9 @@
     <template #heading>lemme in</template>
     <link-base to="/passwords">passwords</link-base>
     <link-base to="/tags">tags</link-base>
+    <client-only>
+      <toggle-theme />
+    </client-only>
   </container>
 </template>
 
