01aa380637a4a9ff39271c14bdee6afb03ee10d7
Omar Hussein <omarelwy@hotmail.com>
2021-10-03 03:52:47 +0200

    🐛 Await for next on security middleware

--- api/middlewares/security.middleware.ts
+++ api/middlewares/security.middleware.ts
@@ -4,8 +4,8 @@ import { Snelm } from "../deps.ts";
 const snelm = new Snelm("oak");
 
 export default function (app: Application) {
-  app.use(({ response, request }, next) => {
-    response = snelm.snelm(request, response);
-    next();
+  app.use(async (ctx, next) => {
+    ctx.response = snelm.snelm(ctx.request, ctx.response);
+    await next();
   });
 }
